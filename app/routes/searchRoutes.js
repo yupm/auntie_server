8a549904e010e0dd53c1eb5b1da71ebcf32be95c
Milton <35495603+yupm@users.noreply.github.com>
2019-01-27 23:23:00 +0800

    Use search query string

--- app/routes/searchRoutes.js
+++ app/routes/searchRoutes.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const Item = mongoose.model('item');
-
+const querystring = require('querystring');
 var elasticsearch = require('elasticsearch');
 var client = new elasticsearch.Client({
     host: 'localhost:9200',
@@ -10,31 +10,14 @@ var client = new elasticsearch.Client({
 
 module.exports = function(app) {
     // SEARCH SECTION =========================
-    app.get('/search', function(req, res) {
-        console.log("In get");
-        console.log(req.query);
-
-         res.render('search.ejs', {
-             user : req.user,
-             results: ""
-         });
-     });
- 
-
-
-
-
-
-     app.post('/search', async(req, res) => {
-        console.log("Searching");
-        console.log(req.body);  
+    app.get('/search',  async(req, res) =>{
 
         const esSearch = await client.search({
             index: 'consumer',
             body: {
               query: {
                 multi_match: {
-                    query: req.body.searchInput, 
+                    query: req.query.q, 
                     fuzziness: 6,
                     prefix_length: 1,
                     fields: [ "tile", "description", "tags"] 
@@ -43,21 +26,14 @@ module.exports = function(app) {
             }
           });
           
-        /*
-        console.log(esSearch);
-        for (const result of esSearch.hits.hits) {
-            console.log('result:', result);
-        }*/
         var obj_ids = esSearch.hits.hits.map(function(searchResult) { return mongoose.Types.ObjectId(searchResult._id); });
         const results = await Item.find({_id: {$in: obj_ids}}).populate('company');
 
-        res.render('search.ejs', {
-            user : req.user,
-            results
-        });
-
-    });
-
+         res.render('search.ejs', {
+             user : req.user,
+             results
+         });
+     });
 
  }
  
