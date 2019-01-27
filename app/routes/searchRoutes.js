41d16b801ea162f76e22f8137656633292d13cd6
Milton <35495603+yupm@users.noreply.github.com>
2019-01-27 12:59:57 +0800

    Elastic search install and guides

--- app/routes/searchRoutes.js
+++ app/routes/searchRoutes.js
@@ -1,8 +1,12 @@
 const mongoose = require('mongoose');
 const Item = mongoose.model('item');
 
-
-
+var elasticsearch = require('elasticsearch');
+var client = new elasticsearch.Client({
+    host: 'localhost:9200',
+    log: 'trace',
+    apiVersion: '5.6'
+  });
 
 module.exports = function(app) {
     // SEARCH SECTION =========================
@@ -18,6 +22,43 @@ module.exports = function(app) {
         console.log("Searching");
         console.log(req.body);  
 
+
+        const esSearch = await client.search({
+            index: 'consumer',
+            body: {
+              query: {
+                multi_match: {
+                    query: req.body.searchInput, 
+                    fuzziness: 2,
+                    prefix_length: 1,
+                    fields: [ "tile", "description", "tags"] 
+                }
+              }
+            }
+          });
+          
+       // const results = await Item.find({ $text: { $search: req.body.searchInput } }).populate('company');
+
+        console.log(esSearch);
+        for (const result of esSearch.hits.hits) {
+            console.log('result:', result);
+        }
+
+
+        res.render('search.ejs', {
+            user : req.user,
+            results: ""
+        });
+
+    });
+ }
+ 
+
+ /*
+     app.post('/search', async(req, res) => {
+        console.log("Searching");
+        console.log(req.body);  
+
         const results = await Item.find({ $text: { $search: req.body.searchInput } }).populate('company');
         console.log(results);
 
@@ -28,5 +69,4 @@ module.exports = function(app) {
         });
 
     });
- }
- 
+*/
